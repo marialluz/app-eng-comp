2ae2b7172ecd66b5162733e9a23a43eef5d8c06e
Mateus Cavalcanti <mateuscavalcanti1607@gmail.com>
2024-10-21 20:00:27 -0300

    fix: enforce email format

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class UserCreate(BaseModel):
-    email: str
+    email: EmailStr
     password: str
     confirm_password: str
 
 
 class UserResponse(BaseModel):
     id: int
-    email: str
+    email: EmailStr
