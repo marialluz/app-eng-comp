7e7ac13268c605351e8094826eb43efe915b1e7f
Mateus Cavalcanti <mateuscavalcanti1607@gmail.com>
2025-01-19 20:53:10 -0300

    fix list permissions in post

--- backend/api/post/permissions.py
+++ backend/api/post/permissions.py
@@ -3,6 +3,9 @@ from rest_framework import permissions
 
 class IsPostByTeacher(permissions.BasePermission):
     def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
         return request.user.is_teacher
 
     def has_object_permission(self, request, view, obj):
